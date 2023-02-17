ecba5a2f8483f601bad51f7e0e7c1ab01e6cdbfa
kollydap <dapkolly@gmail.com>
2023-02-17 15:32:37 +0100

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useRef, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  Alert,
+  ScrollView,
+} from "react-native";
 
 export default function App() {
+  const [inputText, setinputText] = useState("");
+  const [todos, setTodos] = useState([]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.secondlayer}>
+        <TextInput
+          value={inputText}
+          onChangeText={(text) => {
+            setinputText(text);
+          }}
+          style={styles.inputStyles}
+          placeholder="Input a task"
+        />
+        <Button
+          onPress={() => {
+            if (inputText === "") {
+              Alert.alert("Please. Input a task");
+              return;
+            }
+            todos.push(inputText);
+            setTodos([...todos]);
+            setinputText("");
+          }}
+          title="Create"
+        />
+      </View>
+      {todos.map((todo, i) => {
+        return (
+          <View style={styles.thirdlayer} key={i}>
+            <Text style={{ color: "white", fontStyle: "italic" }}>{todo}</Text>
+          </View>
+        );
+      })}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 45,
+    paddingHorizontal: 35,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  inputStyles: {
+    borderWidth: 1,
+    padding: 3,
+    width: "80%",
+    marginRight: 10,
+    borderRadius: 5,
+  },
+  secondlayer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 30,
+    borderBottomColor: "grey",
+    borderBottomWidth: 1,
+    paddingBottom: 30,
+  },
+  thirdlayer: {
+    padding: 10,
+    backgroundColor: "blue",
+    borderRadius: 5,
+    marginBottom: 5,
   },
 });
